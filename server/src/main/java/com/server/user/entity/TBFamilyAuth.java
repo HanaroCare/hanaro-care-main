99e659d44476c82577157d7af4498139fd06f509
zerouriban <minyoung6048@gmail.com>
2026-04-10 10:42:08 +0900

    feat: User entity 생성

---
+++ server/src/main/java/com/server/user/entity/TBFamilyAuth.java
@@ -0,0 +1,74 @@
+package com.server.user.entity;
+
+import com.server.common.entity.BaseEntity;
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "TB_FAMILY_AUTH")
+public class TBFamilyAuth extends BaseEntity {
+
+  @Id
+  @Tsid
+  @Column(name = "FAMILY_AUTH_ID", columnDefinition = "bigint unsigned comment '가족 권한 고유 식별자'")
+  private Long familyAuthId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+      name = "USER_GRANTOR_ID",
+      referencedColumnName = "USER_ID",
+      columnDefinition = "bigint unsigned not null",
+      foreignKey = @ForeignKey(name = "fk_FamilyAuth_userGrantorId_User")
+  )
+  private TBUser grantor;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+      name = "USER_GRANTEE_ID",
+      referencedColumnName = "USER_ID",
+      columnDefinition = "bigint unsigned not null",
+      foreignKey = @ForeignKey(name = "fk_FamilyAuth_userGranteeId_User")
+  )
+  private TBUser grantee;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+      name = "CARD_ID",
+      referencedColumnName = "CARD_ID",
+      columnDefinition = "bigint unsigned",
+      foreignKey = @ForeignKey(name = "fk_FamilyAuth_cardId_Card"))
+  private TBCard card;
+
+  @Column(name = "INS_VIEW_YN", nullable = false, length = 1)
+  private String insViewYn;
+
+  @Column(name = "AUTH_STATUS", nullable = false)
+  private Boolean authStatus; // BOOLEAN (true: 승인, false: 거절/대기)
+
+  @Column(name = "RELATION_CD", nullable = false)
+  private Boolean relationCd; // BOOLEAN (부모/자녀 구분용)
+
+  @Column(name = "CARD_VIEW_YN", nullable = false, length = 1)
+  private String cardViewYn;
+
+
+}
---
+++ server/src/main/java/com/server/user/entity/TBUserLoginLog.java
@@ -0,0 +1,61 @@
+package com.server.user.entity;
+
+import com.server.common.entity.BaseEntity;
+import com.server.user.enums.LoginMeans;
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "TB_USER_LOGIN_LOG")
+public class TBUserLoginLog extends BaseEntity {
+
+  @Id
+  @Tsid
+  @Column(name = "USER_LOG_ID", columnDefinition = "bigint unsigned")
+  private Long userLogId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+      name = "USER_ID",
+      referencedColumnName = "USER_ID",
+      columnDefinition = "bigint unsigned not null",
+      foreignKey = @ForeignKey(name = "fk_UserLoginLog_userId_User")
+  )
+  private TBUser user;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "USER_MEANS_CD", nullable = false, length = 20)
+  private LoginMeans userMeansCd;
+
+  @Builder.Default
+  @Column(name = "USER_RESULT_YN", nullable = false, length = 1)
+  private String userResultYn = "Y";
+
+  @Column(name = "ACCESS_IP_ADDR", length = 50)
+  private String accessIpAddr;
+
+  @Column(name = "ACCESS_DEV_NM", length = 255)
+  private String accessDevNm;
+
+}
