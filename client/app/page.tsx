c052c6d07aabe899d131252afe5d692dea06eef9
KelseyKwon <nykwon7777@gmail.com>
2026-04-10 14:45:30 +0900

    HN-17-feat/그래프 컴포넌트

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
+import { FinancialAssetCard } from './asset/component/FinancialAssetCard';
 import { MedicalBillCard } from './asset/component/MedicalBillCard';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 p-6 font-sans">
       <MedicalBillCard />
+      <FinancialAssetCard />
     </div>
   );
 }
