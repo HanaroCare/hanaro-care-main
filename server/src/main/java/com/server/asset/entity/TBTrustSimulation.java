ece38c2e8a91c4e23de39985cde1f9977179a883
KelseyKwon <nykwon7777@gmail.com>
2026-04-13 19:32:17 +0900

    feat : 엔티티 추가

---
+++ server/src/main/java/com/server/asset/entity/TBTrustSimulation.java
@@ -0,0 +1,88 @@
+package com.server.asset.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.server.asset.entity.enums.InvestType;
+import com.server.asset.entity.enums.PayoutType;
+import com.server.asset.entity.enums.StartType;
+import com.server.common.entity.BaseCreatedEntity;
+import com.server.user.entity.TBUser;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "TB_TRUST_SIMULATION",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			name = "unique_TbTrustSimulation_userId",
+			columnNames = {"USER_ID"}
+		)
+	})
+public class TBTrustSimulation extends BaseCreatedEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "bigint unsigned")
+	private Long trustSimulationId;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "USER_ID",
+		columnDefinition = "bigint unsigned not null",
+		foreignKey = @ForeignKey(name = "fk_TbTrustSimulation_userId_TbUser"))
+	private TBUser user;
+
+	@Column(name = "PRINCIPAL_AMOUNT",
+		precision = 13, scale = 2,
+		nullable = true)
+	private BigDecimal principalAmount;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "START_TYPE", nullable = false)
+	private StartType startType;
+
+	@Column(name = "START_DATE")
+	private LocalDate startDate;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "INVEST_TYPE", nullable = false)
+	private InvestType investType;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "PAYOUT_TYPE", nullable = false)
+	private PayoutType payoutType;
+
+	@Column(name = "PAYOUT_SETTINGS", columnDefinition = "JSON")
+	private String payoutSettings;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CLAIM_AGENT_ID", referencedColumnName = "USER_ID",
+		columnDefinition = "bigint unsigned",
+		nullable = true,
+		foreignKey = @ForeignKey(name = "fk_TrustSimulation_claimAgentId_User"))
+	private TBUser claimAgent;
+}
---
+++ server/src/main/java/com/server/card/entity/TBCard.java
@@ -0,0 +1,60 @@
+package com.server.card.entity;
+
+import java.math.BigDecimal;
+
+import com.server.asset.entity.TBAccount;
+import com.server.common.entity.BaseEntity;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "TB_CARD")
+public class TBCard extends BaseEntity {
+
+	@Id
+	@Tsid
+	@Column(name = "CARD_ID", columnDefinition = "bigint unsigned")
+	private Long cardId;
+
+	@Column(name = "CARD_NM", nullable = false, length = 255)
+	private String cardNm;
+
+	@Column(name = "AUTO_TRANS_AMT", nullable = false, precision = 18, scale = 2)
+	@Builder.Default
+	private BigDecimal autoTransAmt = BigDecimal.ZERO;
+
+	@Column(name = "USE_YN", nullable = false, length = 1)
+	@Builder.Default
+	private String useYn = "Y";
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ACCOUNT_ID",
+		columnDefinition = "bigint unsigned not null",
+		foreignKey = @ForeignKey(name = "fk_Card_accountId_Account"))
+	private TBAccount account;
+
+	@Column(name = "LIMIT_AMT", nullable = false, precision = 18, scale = 2)
+	@Builder.Default
+	private BigDecimal limitAmt = BigDecimal.ZERO;
+
+}
