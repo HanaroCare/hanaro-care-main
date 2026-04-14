52aee4f71189ea4cc3d814397c3cf0d6a327b991
diacond <tiger874587@gmail.com>
2026-04-14 10:17:21 +0900

    HN-74-fix:충ëdevelop asset,user에 필요한 repository, service, dto 추가

--- server/src/main/java/com/server/asset/repository/AccountRepository.java
+++ server/src/main/java/com/server/asset/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.server.asset.repository;
 
 import com.server.asset.entity.TBAccount;
+import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,10 @@ public interface AccountRepository extends JpaRepository<TBAccount, Long> {
 
   @Query("SELECT a FROM TBAccount a WHERE a.user.userId = :userId")
   List<TBAccount> findByUserId(Long userId);
+
+  @Query("SELECT SUM(a.balanceAmt) FROM TBAccount a WHERE a.user.userId = :userId")
+  BigDecimal findTotalBalanceByUserId(Long userId);
+
+  @Query("SELECT a.assetCateCd, SUM(a.balanceAmt) FROM TBAccount a WHERE a.user.userId = :userId GROUP BY a.assetCateCd")
+  List<Object[]> findBalanceSumGroupByCategoryByUserId(Long userId);
 }
--- server/src/main/java/com/server/asset/repository/RealAssetRepository.java
+++ server/src/main/java/com/server/asset/repository/RealAssetRepository.java
@@ -1,6 +1,7 @@
 package com.server.asset.repository;
 
 import com.server.asset.entity.TBRealAsset;
+import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ public interface RealAssetRepository extends JpaRepository<TBRealAsset, Long> {
 
   @Query("SELECT r FROM TBRealAsset r WHERE r.user.userId = :userId")
   List<TBRealAsset> findByUserId(Long userId);
+
+  @Query("SELECT r.assetCateCd, SUM(r.evalAmt) FROM TBRealAsset r WHERE r.user.userId = :userId GROUP BY r.assetCateCd")
+  List<Object[]> findEvalAmtSumGroupByCategoryByUserId(Long userId);
 }
