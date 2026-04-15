0794996ac1b15b0ebf68f31225eea3ef148435e4
sonsy <89962804+s0nsy@users.noreply.github.com>
2026-04-15 17:05:07 +0900

    Hn 61 feat/상속 설계 백엔드 구현 (#41)
    * feat: 유저 로그인 api 개발 및 entity 수정
    
    * refactor: 유저 엔티티 권한 필드 추가
    
    * feat: 인증 핸들러 추가 및 토큰 저장 로직 수정
    
    * fix: Security 인증 객체 타입 불일치 해결 및 커스텀 로그인 필터 추가
    
    * fix: 회원가입 api 개발 및 swaggerConfig 설정
    
    * Merge regist branch into login branch
    
    * feat: 간편 로그인 기능 추가 및 인증 검증 로직 보완
    
    - [인증] PIN, 패턴, FaceID 등 간편 로그인 인증 수단 추가
    - [인증] 커스텀 Validator를 통한 패스워드 및 인증 패턴 검증 로직 구현
    - [보안] 로그인 성공/실패 핸들러 및 로그인 이력 저장 기능 반영
    - [설정] defer-datasource-initialization 활성화로 JPA-SQL 실행 순서 동기화
    - [기타] 리프레시 토큰 및 간편 인증 관련 엔티티/레포지토리 추가
    
    * feat: 집나간 공컴 삭제
    
    * TEMP: 구현 중...
    
    * FEAT: 상속편지 API 설계
    
    * TEMP: 임의후견인 API 설계 중
    
    * TEMP: 보험 관리 API 설계 중
    
    * feat: 빌드 오류 수정
    
    * feat: 빌드 오류 수정
    
    * feat: mysql 삭제
    
    * chore: B도메인 분리
    
    * feat: 스키마 정리
    
    * feat: 가족 비율 요약 조회 api 설계
    
    * feat: 상속 편지 API 기능 마무리
    
    * refactor: 빌드 오류 수정, 코드 정리
    
    * feat: 상속 편지 응답 코드 정리
    
    * feat: 커스텀 어노테이션 추가
    
    * refactor: 코드래빗 피드백 수정
    
    * refactor: 기본 데이터 수정
    
    * refactor: 코드래빗 피드백 수정
    
    * refactor: 코드 정리
    
    * refactor: 코드래빗 피드백 수정
    
    ---------
    
    Co-authored-by: zerouriban <minyoung6048@gmail.com>

---
+++ server/src/main/java/com/server/inheritance/dto/LetterRequestDto.java
@@ -0,0 +1,31 @@
+package com.server.inheritance.dto;
+
+import com.server.inheritance.dto.validatior.ValidLetterRequest;
+import com.server.inheritance.enums.LetterType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ValidLetterRequest
+public class LetterRequestDto {
+
+  @NotNull(message = "상속 상세 ID는 필수입니다.")
+  @Schema(description = "상속 상세 ID", example = "3")
+  private Long inheritDetailId;
+
+  @NotNull(message = "편지 타입은 필수입니다.")
+  @Schema(description = "편지 타입", example = "WRITING")
+  private LetterType letterTypeCd;
+
+  @Schema(description = "편지 내용", example = "사랑하는 자녀에게...")
+  private String letterCont;
+}
