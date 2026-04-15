d67c5767e3a5866678273ce11b076fb1232305cd
KelseyKwon <nykwon7777@gmail.com>
2026-04-15 14:17:45 +0900

    HN-90-feat/middleware 인증 redirect 활성화 - HttpOnly 쿠키 기반 접근 제어 적용

--- client/middleware.ts
+++ client/middleware.ts
@@ -9,15 +9,13 @@ export function middleware(request: NextRequest) {
 
   const isPublic = PUBLIC_PATHS.some((path) => pathname.startsWith(path));
 
-  // TODO: 로그인이 HttpOnly 쿠키 방식으로 전환된 후 주석 해제
-  // if (!token && !isPublic) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (!token && !isPublic) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  // 정적 파일, 이미지, Next.js 내부 경로 제외
   matcher: ['/((?!_next/static|_next/image|favicon.ico|images).*)'],
 };
