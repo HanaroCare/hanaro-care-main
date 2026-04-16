c108f5e58a17c4971a417382281ab48b5ab24300
KelseyKwon <nykwon7777@gmail.com>
2026-04-16 10:55:11 +0900

    HN-91-feat/의료비 계산 로직 수정

--- server/src/main/java/com/server/asset/service/SimulationEngine.java
+++ server/src/main/java/com/server/asset/service/SimulationEngine.java
@@ -28,8 +28,8 @@ public class SimulationEngine {
     private final BokjiroClient bokjiroClient;
 
     private static final BigDecimal AVG_WAGE_GROWTH = new BigDecimal("2.9"); // 2.9%
-    private static final BigDecimal SENIOR_MEDICAL_COST_YEAR = new BigDecimal("6000000");
-    private static final BigDecimal MEDICAL_INFLATION = new BigDecimal("4.5"); // 4.5%
+    private static final BigDecimal SENIOR_MEDICAL_COST_YEAR = new BigDecimal("1200000"); // 개인부담 기준 월 10만원 × 12
+    private static final BigDecimal MEDICAL_INFLATION = new BigDecimal("3.0"); // 3.0%
 
     public SimulationDetailResponse run(AIAnalysisInput input) {
         // 1. 연금 추정 결과(금액 + 연동여부) 가져오기
@@ -114,7 +114,8 @@ public class SimulationEngine {
             if (segStart >= 80) segLiving = segLiving.multiply(new BigDecimal("0.8")).setScale(0, RoundingMode.HALF_UP);
 
             BigDecimal medicalFactor = BigDecimal.valueOf(Math.pow(medicalGrowthRate, yearsFromNow));
-            if (segStart >= 75) medicalFactor = medicalFactor.multiply(new BigDecimal("1.5"));
+            if (segStart >= 85) medicalFactor = medicalFactor.multiply(new BigDecimal("1.5"));
+            else if (segStart >= 75) medicalFactor = medicalFactor.multiply(new BigDecimal("1.3"));
             BigDecimal segMedical = monthlyMedicalBase.multiply(medicalFactor).setScale(0, RoundingMode.HALF_UP);
 
             BigDecimal segCare = (segStart < 75) ? BigDecimal.ZERO :
