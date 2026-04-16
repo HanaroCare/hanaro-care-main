4a4f5ede3a0321cfe33541651a9c6be6313dc65e
KelseyKwon <nykwon7777@gmail.com>
2026-04-16 17:40:06 +0900

    HN-105-fix/원복

--- server/src/main/java/com/server/asset/controller/AssetController.java
+++ server/src/main/java/com/server/asset/controller/AssetController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -93,4 +95,23 @@ public class AssetController {
 	) {
 		return ApiResponse.onSuccess(assetService.getInsuranceAssets(subscriberDTO.getUserId()));
 	}
+
+	@Operation(
+		summary = "금융 자산 연동 상태 변경",
+		description = "선택한 금융 계좌들의 연동 여부를 업데이트합니다.",
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			content = @Content(
+				examples = @ExampleObject(value = "[2011, 2014]")
+			)
+		)
+	)
+	@PatchMapping("/link")
+	public ApiResponse<String> updateAssetLinkStatus(
+		@AuthenticationPrincipal SubscriberDTO subscriberDTO,
+		@RequestBody List<Long> accountIds
+	) {
+		assetService.updateAssetLinkStatus(subscriberDTO.getUserId(), accountIds);
+		return ApiResponse.onSuccess("자산 연동 설정이 변경되었습니다.");
+	}
+
 }
--- server/src/main/java/com/server/asset/controller/LinkController.java
+++ server/src/main/java/com/server/asset/controller/LinkController.java
@@ -1,10 +1,7 @@
 package com.server.asset.controller;
 
-import java.util.List;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +15,6 @@ import com.server.common.response.ApiResponse;
 import com.server.common.security.dto.SubscriberDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -30,25 +25,6 @@ import lombok.RequiredArgsConstructor;
 public class LinkController {
 
 	private final RealAssetService realAssetService;
-	private final AssetService assetService;
-
-	@Operation(
-		summary = "금융 자산 연동 상태 변경",
-		description = "선택한 금융 계좌들의 연동 여부를 업데이트합니다.",
-		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-			content = @Content(
-				examples = @ExampleObject(value = "[2011, 2014]")
-			)
-		)
-	)
-	@PatchMapping("/link")
-	public ApiResponse<String> updateAssetLinkStatus(
-		@AuthenticationPrincipal SubscriberDTO subscriberDTO,
-		@RequestBody List<Long> accountIds
-	) {
-		assetService.updateAssetLinkStatus(subscriberDTO.getUserId(), accountIds);
-		return ApiResponse.onSuccess("자산 연동 설정이 변경되었습니다.");
-	}
 
 	@Operation(summary = "부동산 연동")
 	@PostMapping("/link/housing")
