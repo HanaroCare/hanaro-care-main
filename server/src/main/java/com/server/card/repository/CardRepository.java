10699c4bfdea94c3e0a1844392a947642ea74b28
SeolJiyun <85937340+SeolJiyun@users.noreply.github.com>
2026-04-17 15:11:40 +0900

    머지 후 build error 해결
    CardRepository 타입 추가

--- server/src/main/java/com/server/card/repository/CardRepository.java
+++ server/src/main/java/com/server/card/repository/CardRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CardRepository extends JpaRepository<TBCard, Long> {
 
   List<TBCard> findAllByAccount_User_UserId(Long userId);
+
+  List<TBCard> findByAccount_User_UserIdAndIsUseTrue(Long userId);
 }
