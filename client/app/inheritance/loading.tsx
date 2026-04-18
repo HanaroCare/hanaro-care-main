91877e7e1d9484b30e274007abdc5379d86148bb
seyoung <s3young.dev@gmail.com>
2026-04-18 17:25:38 +0900

    refactor: /inheritance 설정

---
+++ client/app/inheritance/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="animate-pulse font-medium text-hana-ez-600">
+        상속 설계 설정을 확인 중입니다...
+      </div>
+    </div>
+  );
+}
