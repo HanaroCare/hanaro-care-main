b6ea18e21eef39458822d6cb5a225e663ea4bff3
sonsy <89962804+s0nsy@users.noreply.github.com>
2026-04-18 15:54:09 +0900

    Revert "Revert "refactor: QA 반영 (#83)" (#86)"
    This reverts commit 070e230a49a1de9b70c42cde06e7ae9cefd14ad4.

--- client/app/my/guardian/page.tsx
+++ client/app/my/guardian/page.tsx
@@ -4,6 +4,8 @@ import { Suspense, useState } from 'react';
 import GuardianStepRenderer from './components/StepRenderer';
 import { useGuardianStep } from './hooks/useGuardianStep';
 import type { GuardianData } from './types/types';
+import { findId } from '@/app/(auth)/login/actions/user';
+import { getUserName } from '../actions/guardianActions';
 
 const TOTAL_STEPS = 7;
 
@@ -11,13 +13,12 @@ function GuardianContent() {
   const { currentStep, next, prev, goTo } = useGuardianStep(TOTAL_STEPS);
   const [data, setData] = useState<GuardianData>({
     selectedPerson: null,
-    relationship: '',
+    relationship: '', 
     permissions: [false, false, false, false, false],
     userName: '',
     userPhone: '',
     verificationMethod: 'phone',
   });
-
   const updateData = (updates: Partial<GuardianData>) =>
     setData((d) => ({ ...d, ...updates }));
 
